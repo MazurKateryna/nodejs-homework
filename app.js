464683b93b47ffffeddd7d899a3f53d4bd0ae1b3
MazurKateryna <myEmail@example.com>
2021-05-04 12:28:28 +0300

    fix users

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const { HttpCode } = require('./helpers/constants')
 
+const usersRouter = require('./routes/api/users')
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
+app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: 10000 }))
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  handler: (req, res, next) => {
+    return res.status(HttpCode.BAD_REQUEST).json({
+      status: 'error',
+      code: HttpCode.BAD_REQUEST,
+      data: 'Bad request',
+      message: 'Too many requests, please try again later.',
+    })
+  },
+})
 
+app.use('/api/', apiLimiter)
+app.use('/api/users', usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((_req, res) => {
@@ -23,3 +42,30 @@ app.use((err, _req, res, _next) => {
 })
 
 module.exports = app
+// const express = require('express')
+// const logger = require('morgan')
+// const cors = require('cors')
+
+// const usersRouter = require('./routes/api/users')
+// const contactsRouter = require('./routes/api/contacts')
+
+// const app = express()
+
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+// app.use(logger(formatsLogger))
+// app.use(cors())
+// app.use(express.json())
+
+// app.use('/api/users', usersRouter)
+// app.use('/api/contacts', contactsRouter)
+
+// app.use((_req, res) => {
+//   res.status(404).json({ message: 'Not found' })
+// })
+
+// app.use((err, _req, res, _next) => {
+//   res.status(err.status || 500).json({ message: err.message })
+// })
+
+// module.exports = app
